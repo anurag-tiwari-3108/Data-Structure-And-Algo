b9a06d83936efab5bf596e2cbfcf737fcd4aa0a7
Anurag Tiwari <anuragtiwari@Anurags-MacBook-Air.local>
2025-05-21 12:49:09 +0100

    update system design codes

--- src/com/codex/dsa/arrays/SubsetSum2.java
+++ src/com/codex/dsa/arrays/SubsetSum2.java
@@ -5,31 +5,33 @@ import java.util.Arrays;
 import java.util.List;
 
 // Subset Sum 1 prob with one additional condition to avoid any duplicate
-// One things which we can do is to put the response into set but that will increase the time and space complexity
-
 // We are following another approach here to avoid duplicate i.e sort the array and validate if current_val == current-1_val
 public class SubsetSum2 {
 
-	private void subsetSum(int[] arr, int N) {
-		Arrays.sort(arr);
-		List<List<Integer>> ans = new ArrayList<>();
-		subsetSum(0, arr, new ArrayList<>(), ans);
+    public List<List<Integer>> subsetSum(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(0, arr, new ArrayList<>(), ans);
+        return ans;
+    }
 
-	}
+    private void backtrack(int ind, int[] arr, List<Integer> ds, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(ds));
+        for (int i = ind; i < arr.length; i++) {
+            if (i != ind && arr[i] == arr[i - 1]) continue;
+            ds.add(arr[i]);
+            backtrack(i + 1, arr, ds, ans);
+            ds.remove(ds.size() - 1);
+        }
+    }
 
-	private void subsetSum(int ind, int[] arr, List ds, List<List<Integer>> ans) {
-
-		ans.add(new ArrayList<>(ds));
-		for (int i = ind; i < arr.length; i++) {
-			if (i != ind && arr[i] == arr[i - 1])
-				continue;
-
-			ds.add(arr[i]);
-			subsetSum(ind + 1, arr, ds, ans);
-			ds.remove(ds.size() - 1);
-
-		}
-
-	}
-
+    public static void main(String[] args) {
+        SubsetSum2 solver = new SubsetSum2();
+        int[] arr = {1, 2, 2};
+        List<List<Integer>> result = solver.subsetSum(arr);
+        System.out.println("All unique subsets:");
+        for (List<Integer> subset : result) {
+            System.out.println(subset);
+        }
+    }
 }
